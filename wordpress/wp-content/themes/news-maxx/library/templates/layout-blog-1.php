40228df4ab2af60581922e36b940bca7a64152ee
melmiranda <melania.smc@hotmail.com>
2017-05-31 16:46:57 -0300

    16:50 31-05-2017

--- wordpress/wp-content/themes/news-maxx/library/templates/layout-blog-1.php
+++ wordpress/wp-content/themes/news-maxx/library/templates/layout-blog-1.php
@@ -58,54 +58,149 @@
 
       <style >
 
+        .first-left-box-inner h1{
+          font-family: "Condensed-bold-italic";
+          font-size: 3em;
+          color:white;
+        }
+
+        .first-left-box-inner > div{
+          padding-left: 0;
+          padding-right: 0;
+        }
+
+        .first-left-box-inner .tipo-partido{
+          color: white;
+          font-size: 0.9em;
+        }
+
+        .first-left-box-inner .puntos-partido{
+          color: #00b643;
+          font-size: 4em;
+          text-shadow: -1px 3px 0px rgba(255, 255, 255, 1);
+          font-family: "Condensed-bold-italic";
+        }
+        .first-left-box-inner .level-partido{
+          color:  #00b643;
+          padding: 10px 0;
+          font-family: "Condensed-bold";
+        }
+
+        .first-left-box-inner .level-partido span{
+          color:  #a43c93;
+        }
+
+        .first-left-box-inner > p{
+          font-family: "Roboto-Regular";
+          font-size: .9em;
+          color: white;
+        }
+
+        .first-left-box-inner > p{
+          font-family: "Roboto-Regular";
+          font-size: .9em;
+        }
+
         .linea-divisoria-partido{
           border-bottom: 2px dashed white;
+          margin:20px 0;
         }
 
         .goles span {
           background-color: rgba(0,0,0,.20);
           color:#00b643;
+          padding: 5px;
+          min-width: 50px;
         }
 
         .goles{
           color:white;
         }
 
+
+
       </style>
 
     </div>
 
 
-
-
-      <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 fist-right-box">
-
-      </div>
   </div>
 
-  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 second-right-box">
+  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 first-right-box">
+    <h1>Historial</h1>
+
+    <p>Campeonatos Locales</p>
+        <table style="width:100%">
+          <tr>
+            <th></th>
+            <th>Partidos jugados <br> <span>1049</span></th>
+            <th>Ganados<br>  <span>1049</span></th>
+            <th>Empatados<br>  <span>1049</span></th>
+            <th>Perdidos<br>  <span>1049</span></th>
+            <th>Goles a favor <br>  <span>1049</span></th>
+            <th>Goles en contra<br>  <span>1049</span></th>
+
+          </tr>
+          <tr>
+            <td>Local</td>
+            <td>148</td>
+            <td>50</td>
+            <td>148</td>
+            <td>50</td>
+            <td>148</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Visitante</td>
+            <td>148</td>
+            <td>50</td>
+            <td>148</td>
+            <td>50</td>
+            <td>148</td>
+            <td>50</td>
+          </tr>
+        </table>
+
 
   </div>
+
+  <style media="screen">
+    .first-right-box table{
+      border: none;
+      border-top: 1px solid #006443;
+    }
+
+    .first-right-box table tr td{
+      border: none;
+    }
+
+    .first-right-box h1{
+      
+      color: white;
+      font-family: 'Condensed-bold-italic';
+    }
+
+  </style>
 
 
 
 
 </div>
-
+<!--
 
 <div class="sidebar widget-area-2 pull-left">
     <?php
-    if ( is_active_sidebar( $sidebars[$kopa_position[0]] ) ) {
-        dynamic_sidebar($sidebars[$kopa_position[0]]);
-    }
+  //  if ( is_active_sidebar( $sidebars[$kopa_position[0]] ) ) {
+  //      dynamic_sidebar($sidebars[$kopa_position[0]]);
+  //  }
     ?>
 
 <div class="widget-area-7 pull-left">
 
     <?php
-    if ( is_active_sidebar( $sidebars[$kopa_position[1]] ) ) {
-        dynamic_sidebar($sidebars[$kopa_position[1]]);
-    }
+      //if ( is_active_sidebar( $sidebars[$kopa_position[1]] ) ) {
+      //  dynamic_sidebar($sidebars[$kopa_position[1]]);
+      //}
     ?>
 
 </div>
@@ -114,10 +209,10 @@
 <div class="widget-area-8 pull-left">
 
     <?php
-    if ( is_active_sidebar( $sidebars[$kopa_position[2]] ) ) {
+  //  if ( is_active_sidebar( $sidebars[$kopa_position[2]] ) ) {
         dynamic_sidebar($sidebars[$kopa_position[2]]);
-    }
-    ?>
+  //  }
+  //  ?>
 
 </div>
 
@@ -125,12 +220,12 @@
 <div class="clear"></div>
 
     <?php
-    if ( is_active_sidebar( $sidebars[$kopa_position[3]] ) ) {
-        dynamic_sidebar($sidebars[$kopa_position[3]]);
-    }
+    //if ( is_active_sidebar( $sidebars[$kopa_position[3]] ) ) {
+    //    dynamic_sidebar($sidebars[$kopa_position[3]]);
+    //}
     ?>
 
-</div>
+</div> -->
 
 
 <div class="clear"></div>
