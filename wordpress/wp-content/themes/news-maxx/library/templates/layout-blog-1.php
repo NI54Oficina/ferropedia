973d8407d80747bc86395f176764870c7b7607d6
melmiranda <melania.smc@hotmail.com>
2017-06-08 14:48:10 -0300

    14:48 8-6-2017

--- wordpress/wp-content/themes/news-maxx/library/templates/layout-blog-1.php
+++ wordpress/wp-content/themes/news-maxx/library/templates/layout-blog-1.php
@@ -141,69 +141,6 @@
   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 second-box bloque">
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 second-box-left">
 
-      <style media="screen">
-
-
-        .second-box-left .box-left{
-          text-align: center;
-        }
-
-        .second-box-left .box-left img{
-          width: 80%;
-          border: 1px solid black;
-        }
-
-        .second-box-left .box-left p{
-          color: white;
-          font-family: 'Condensed-bold-italic';
-          font-size: 1.6em;
-          padding: 20px 0;
-        }
-
-        .second-box-left .box-left button{
-          color:white;
-          background-color: black;
-          font-family: 'Roboto-regular';
-          border: none;
-          text-decoration: none;
-          padding: 5px 20px;
-        }
-
-        .second-box-left .box-right{
-          border-left: 1px solid black;
-          padding-left: 20px;
-        }
-
-        .second-box-left .box-right p{
-          border-bottom: 1px dashed black;
-          color: white;
-          padding: 15px 0;
-          font-size: 1.2em;
-          font-family: 'Condensed-regular';
-          margin-bottom: 0;
-        }
-
-        .box-jugador{
-          padding: 5px;
-        }
-        .box-jugador > div{
-          background-color: #a43c93;
-          width: 100%;
-          height: 150px;
-          position: relative;
-        }
-
-        .box-jugador label{
-          position: absolute;
-          bottom: 0;
-          width: 100%;
-          background-color: black;
-          color:white;
-          margin-bottom: 0;
-          text-align: center;
-        }
-      </style>
-
       <h1>Los jugadores de Ferro</h1>
 
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 box-left">
@@ -265,50 +202,17 @@
   </div>
 
 </div>
-  <style media="screen">
-
-    .third-box{
-      padding-left: 2em;
-      padding-right: 2em;
-    }
-    .third-box-left{
-      border: 1px solid white;
-      padding: 0;
-    }
-
-    .third-box-left > div{
-      padding: 0;
-      margin: 0;
-    }
-
-    .third-box-left > div > img{
-      width: 100%;
-      height: 100%;
-      padding: 0;
-      margin: 0;
-    }
-
-    .triangulo-verde{
-
-      top: 0;
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      background: linear-gradient(139deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 29%, rgba(0,0,0,0) 49%, rgba(0,182,67,.3) 50%, rgba(0,182,67,.3) 80%, rgba(0,182,67,.3) 100%);
-    }
-
-  </style>
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 third-box bloque">
 
   <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 third-box-left">
 
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-      <img src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo.png" alt="">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 box-left">
+      <img src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
       <div class="triangulo-verde"></div>
     </div>
 
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 ">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 box-right">
       <h3> El rincon del mudo</h3>
       <h1> Siempre igual, Todo Igual, Todo lo mismo</h1>
       <p>En el ámbito del deporte y porque no en unos cuanto más, se ha acuñado el término “panquequear” y “panqueque”. Del panqueque la definición es “Dícese de aquella persona...</p>
@@ -319,10 +223,178 @@
   </div>
 
   <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 third-box-right twitter-widget">
-    //twitter widget
+    //WIDGET TWITTER
   </div>
 
 </div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 fourth-box bloque">
+
+
+  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 fourth-box-left">
+
+    <h1>MUSEO</h1>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 fourth-container">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding">
+        <img src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
+        <div class="triangulo-verde"></div>
+      </div>
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-container">
+        <p>titulo del objeto</p>
+
+        <label>14 de Febrero</label>
+      </div>
+
+    </div>
+
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 fourth-container">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding">
+        <img src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
+        <div class="triangulo-verde"></div>
+      </div>
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-container">
+        <p>titulo del objeto</p>
+
+        <label>14 de Febrero</label>
+      </div>
+
+    </div>
+
+
+
+  </div>
+
+  <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 fourth-box-right twitter-widget">
+    //MÀS VISTO
+  </div>
+
+</div>
+
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 fifth-box bloque"></div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 six-box bloque">
+  <style media="screen">
+    .six-box .six-box-left h1{
+      background-color: #006443;
+      color:white;
+      border-bottom: 3px solid black;
+    }
+
+    .six-container-left > div  img{
+      height: 250px;
+    }
+
+    .six-container-left .title-container{
+      background-color: #006443;
+      color:white;
+      padding-top: 20px;
+      padding-left: 0;
+      height: 200px;
+    }
+
+    .six-container-left .title-container p:first-child{
+      font-family: 'Condensed-bold-italic';
+      font-size: 2em;
+    }
+
+    .six-container-left .title-container p:last-child{
+      font-family: 'Roboto-regular';
+      font-size: 1em;
+    }
+
+    .six-container-left .title-container label{
+      background-color: black;
+      color:#a43c93;
+      position: absolute;
+      bottom:0;
+      width: 100%;
+      margin: 0;
+      padding: 10px;
+    }
+
+    .six-container-right .six-inner-container{
+      padding: 15px 0;
+    }
+
+    .six-container-right .six-inner-container img{
+      /*width: 100%;*/
+      height:150px;
+      display: inline;
+    }
+
+    .six-container-right .six-inner-container p{
+      color:white;
+    }
+
+    .six-container-right .six-inner-container p span{
+      color:#a43c93;
+    }
+    
+
+
+  </style>
+
+  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 six-box-left">
+
+    <h1>De chiquito mi viejo</h1>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 six-container-left">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 no-padding">
+        <img src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
+        <div class="triangulo-verde"></div>
+      </div>
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-container">
+        <p>titulo del objeto</p>
+
+        <p>Esta carta es la historia presente del Club Ferro Carril Oeste. Dedicada...</p>
+
+        <label>14 de Febrero</label>
+      </div>
+
+    </div>
+
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 six-container-right">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 six-inner-container">
+        <img class="col-lg-5 col-md-5 col-sm-5 col-xs-5" src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
+        <p class="col-lg-7 col-md-7 col-sm-7 col-xs-7">¡Gracias por todo, Ferro! Hasta la victoria, siemprebr <br>  <span> 17 de febrero</span></p>
+      </div>
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 six-inner-container">
+        <img class="col-lg-5 col-md-5 col-sm-5 col-xs-5" src="<?php echo site_url(); ?>/wp-content/themes/news-maxx/img/ejemplo-2.png" alt="">
+        <p class="col-lg-7 col-md-7 col-sm-7 col-xs-7">¡Gracias por todo, Ferro! Hasta la victoria, siemprebr <br>  <span> 17 de febrero</span></p>
+      </div>
+
+    </div>
+
+
+
+
+  </div>
+
+  <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 six-box-right twitter-widget">
+    //MÀS VISTO
+  </div>
+
+</div>
+
+
+
+<style media="screen">
+  .seven-box{
+
+  }
+</style>
 <!--
 
 <div class="sidebar widget-area-2 pull-left">
