48256090709cb7a170bd81939eb34f6e65c249e5
melmiranda <melania.smc@hotmail.com>
2017-06-29 13:52:41 -0300

    14:00

--- wordpress/wp-content/themes/news-maxx/functions.php
+++ wordpress/wp-content/themes/news-maxx/functions.php
@@ -20,3 +20,37 @@ wp_enqueue_script( 'script-js', site_url() . '/script.js');
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_add_query' );
+
+function pw_show_gallery_image_urls( $content ) {
+
+ global $post;
+
+ // Only do this on singular items
+ if( ! is_singular() )
+   return $content;
+
+ // Make sure the post has a gallery in it
+ if( ! has_shortcode( $post->post_content, 'gallery' ) )
+   return $content;
+
+ // Retrieve the first gallery in the post
+ $gallery = get_post_gallery_images( $post );
+
+ $image_list = '<ul>';
+
+ // Loop through each image in each gallery
+ foreach( $gallery as $image_url ) {
+
+   $image_list .= '<li>' . '<img src="' . $image_url . '">' . '</li>';
+
+ }
+
+ $image_list .= '</ul>';
+
+ // Append our image list to the content of our post
+ $content .= $image_list;
+
+ return $content;
+
+}
+add_filter( 'the_content', 'pw_show_gallery_image_urls' );
--- wordpress/wp-content/themes/news-maxx/library/templates/template-galeria.php
+++ wordpress/wp-content/themes/news-maxx/library/templates/template-galeria.php
@@ -20,8 +20,18 @@ if ( is_page(get_the_ID()) && have_posts() ) {
 
 
 
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <?php the_content(); ?>
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-gallery">
+
+    <h1>Galeria de Imágenes</h1>
+    <?php the_content(); ?>
+      <?php $img= get_post_galleries_images();
+
+      foreach ($img[0] as $i) {
+
+      //  echo $i;
+      }?>
+
+
   </div>
   <div class="wrapper clearfix">
 
